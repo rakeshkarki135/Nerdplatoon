d47d94e58a2a986e8ef11b552e66f1706d3e5ef5
Rakesh Karki <rakeshkarki135@gmail.com>
2024-11-20 11:23:15 +0545

    changed el-salvador

--- El-salvador/scrapers/insert_to_db.py
+++ El-salvador/scrapers/insert_to_db.py
@@ -19,6 +19,7 @@ def data_cleaning(df: pd.DataFrame) -> pd.DataFrame:
     """
     # Replace NaN values with None for all columns
     # df = df.apply(lambda x: None if pd.isna(x) else x)
+    df['updated_at'] = None
     df = df.replace({pd.NA: None, np.nan: None})
 
     return df
